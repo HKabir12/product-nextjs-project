80163e9d4527db5516bdd258be6a3327124b038c
Humaon Kabir <humaonkabir2003@gmail.com>
2025-08-22 20:01:56 +0600

    google login

--- src/lib/authOptions.js
+++ src/lib/authOptions.js
@@ -39,8 +39,8 @@ export const authOptions = {
 
     // 2️⃣ Google OAuth
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
     }),
   ],
 
